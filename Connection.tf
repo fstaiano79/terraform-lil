7f31b9806415bfa3a99f76fe4c292f5191f29e92
fstaiano79 <fstaiano79@gmail.com>
2019-10-13 15:37:17 +0100

    Building process

--- Connection.tf
+++ Connection.tf
@@ -1,11 +1,11 @@
 provider "azurerm" {
-  subscription_id = var.SUBSCRIPTION_ID
-  client_id = var.CLIENT_ID
-  client_secret = var.CLIENT_SECRET
-  tenant_id = var.TENANT_ID
+  subscription_id = "${var.subscription_id}"
+  client_id = "${var.client_id}"
+  client_secret = "${var.client_secret}"
+  tenant_id = "${var.tenant_id}"
 }
 
-variable "SUBSCRIPTION_ID" {}
-variable "CLIENT_ID" {}
-variable "CLIENT_SECRET" {}
-variable "TENANT_ID" {}
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
