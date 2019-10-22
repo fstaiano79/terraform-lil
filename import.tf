89df5efe43488d0a9f8d2c1bab7b4867e1c93bd0
fstaiano79 <fstaiano79@gmail.com>
2019-10-22 13:05:41 +0100

    Building process

---
+++ import.tf
@@ -0,0 +1,11 @@
+resource "azurerm_resource_group" "vNet30_Securus"{
+  name="vNet30_Securus"
+  location = "North Europe"
+}
+
+resource "azurerm_virtual_network" "vNet30_Securus" {
+  address_space = ["10.0.30.0/24"]
+  name = "vNet30_Securus"
+  location = "North Europe"
+  resource_group_name = "${azurerm_resource_group.vNet30_Securus.name}"
+}
