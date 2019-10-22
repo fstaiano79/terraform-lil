6c6669021d50b560e765407efe72bc78022cef56
fstaiano79 <fstaiano79@gmail.com>
2019-10-22 16:12:02 +0100

    Building process

--- import.tf
+++ import.tf
@@ -8,4 +8,10 @@ resource "azurerm_virtual_network" "vNet30_Securus"{
   name = "vNet30_Securus"
   location = "${azurerm_resource_group.vNet30_Securus.location}"
   resource_group_name = "${azurerm_resource_group.vNet30_Securus.name}"
+  dns_servers = ["192.168.1.200","10.0.10.150","10.0.30.200"]
+}
+
+resource "azurerm_resource_group" "LRS_Services"{
+  name="LRS_Services"
+  location="North Europe"
 }
